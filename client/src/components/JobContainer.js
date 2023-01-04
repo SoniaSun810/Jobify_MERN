161fde2a2e5123b8ecb88ee8ba7d9c915aca667f
Fengnan <soniasun0925@gmail.com>
2023-01-03 22:00:41 -0800

    Add Pagination backend.

--- client/src/components/JobContainer.js
+++ client/src/components/JobContainer.js
@@ -1,49 +1,49 @@
-import { useEffect } from "react";
-import Loading from "./Loading";
-import Job from "./Job";
-import Wrapper from "../assets/wrappers/JobsContainer";
-import { useAppContext } from "../context/appContext";
+// import { useEffect } from "react";
+// import Loading from "./Loading";
+// import Job from "./Job";
+// import Wrapper from "../assets/wrappers/JobsContainer";
+// import { useAppContext } from "../context/appContext";
 
-const JobContainer = () => {
-  const {
-    getJobs,
-    jobs,
-    isLoading,
-    totalJobs,
-    search,
-    searchStatus,
-    searchType,
-    sort,
-  } = useAppContext();
+// const JobContainer = () => {
+//   const {
+//     getJobs,
+//     jobs,
+//     isLoading,
+//     totalJobs,
+//     search,
+//     searchStatus,
+//     searchType,
+//     sort,
+//   } = useAppContext();
 
-  useEffect(() => {
-    getJobs();
-  }, [search, searchStatus, searchType, sort]);
+//   useEffect(() => {
+//     getJobs();
+//   }, [search, searchStatus, searchType, sort]);
 
-  if (isLoading) {
-    return <Loading center />;
-  }
-  if (jobs.length === 0) {
-    return (
-      <Wrapper>
-        <h2>No jobs to display...</h2>
-      </Wrapper>
-    );
-  }
+//   if (isLoading) {
+//     return <Loading center />;
+//   }
+//   if (jobs.length === 0) {
+//     return (
+//       <Wrapper>
+//         <h2>No jobs to display...</h2>
+//       </Wrapper>
+//     );
+//   }
 
-  return (
-    <Wrapper>
-      <h5>
-        {totalJobs} job{jobs.length > 1 && "s"} found
-      </h5>
-      <div className="jobs">
-        {jobs.map((job) => {
-          return <Job key={job._id} {...job} />;
-        })}
-      </div>
-      {/* pagination buttons */}
-    </Wrapper>
-  );
-};
+//   return (
+//     <Wrapper>
+//       <h5>
+//         {totalJobs} job{jobs.length > 1 && "s"} found
+//       </h5>
+//       <div className="jobs">
+//         {jobs.map((job) => {
+//           return <Job key={job._id} {...job} />;
+//         })}
+//       </div>
+//       {/* pagination buttons */}
+//     </Wrapper>
+//   );
+// };
 
-export default JobContainer;
+// export default JobContainer;
--- controller/jobsController.js
+++ controller/jobsController.js
@@ -35,29 +35,36 @@ const getAllJobs = async (req, res) => {
   if (search) {
     queryObject.position = { $regex: search, $options: "i" };
   }
-  
+
   // NO AWAIT
   let result = Job.find(queryObject);
   // Chain sort conditions
-  if(sort === 'latest'){
-    result = result.sort('-createdAt')
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
   }
-  if(sort === 'oldest'){
-    result = result.sort('createdAt')
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
   }
-  if(sort === 'a-z'){
-    result = result.sort('position')
+  if (sort === "a-z") {
+    result = result.sort("position");
   }
-  if(sort === 'z-a'){
-    result = result.sort('-position')
+  if (sort === "z-a") {
+    result = result.sort("-position");
   }
 
+  // set up pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const jobs = await result;
 
-  res
-    .status(StatusCodes.OK)
-    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const updateJob = async (req, res) => {
